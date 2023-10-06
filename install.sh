275b4bd0a99fedfbdf399188e53157225008b534
rupi <rupi@mmonstar.co.kr>
2023-10-06 10:16:21 +0900

    rupi-office: added install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# docker install
+# docker-compose install
+# git clone git@github.com:m-monstar/mmon-docker-std.git mmon-solution
+# git clone git@github.com:m-monstar/mmon-v2-scm.git mmon-solution/src/scm
+# git clone git@github.com:m-monstar/mmon-v2-backend.git mmon-solution/src/backend
+# git clone git@github.com:m-monstar/mmon-v2-partnershop-api.git mmon-solution/src/api
+# cd mmon-solution/src/scm
+# git submodule update --init
+# cd ../api
+# git submodule update --init
+# cd ../backend
+# git submodule update --init
+# cd ../../
+# docker compose build
+# docker compose up
+# docker compose up -d
+# docker compose stop
+# docker compose start
