cafd63157a75e428699c4c76aafadf0c5ba91002
kimreo@mmonstar.co.kr <kimreo@mmonstar.co.kr>
2023-10-10 08:04:14 +0900

    명령어 전 존재 유무 확인 추가

--- compose-init.sh
+++ compose-init.sh
@@ -13,6 +13,12 @@ docker compose build
 docker compose up -d
 
 # Compose install In Docker Container
-docker exec -w /var/www/$SCM_DIR -it $(docker container ls -a -q --filter "name=^$PROJECT_DIR-$SCM_DIR") sh -c "composer install"
+if [ $(docker container ls -a -q --filter "name=^$PROJECT_DIR-$SCM_DIR") ]: then
+    docker exec -w /var/www/$SCM_DIR -it $(docker container ls -a -q --filter "name=^$PROJECT_DIR-$SCM_DIR") sh -c "composer install"
+fi
+if [ $(docker container ls -a -q --filter "name=^$PROJECT_DIR-$API_DIR") ]: then
 docker exec -w /var/www/$API_DIR -it $(docker container ls -a -q --filter "name=^$PROJECT_DIR-$API_DIR") sh -c "composer install"
+fi
+if [ $(docker container ls -a -q --filter "name=^$PROJECT_DIR-$BACKEND_DIR") ]: then
 docker exec -w /var/www/$BACKEND_DIR -it $(docker container ls -a -q --filter "name=^$PROJECT_DIR-$BACKEND_DIR") sh -c "composer install"
+fi
