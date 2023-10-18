e54c9a58c805a0298490e187e0fa8b54242512fc
rupi <rupi@mmonstar.co.kr>
2023-10-18 14:50:59 +0900

    create_user sql modified

--- initdb.d/99-create_user.sql
+++ initdb.d/99-create_user.sql
@@ -3,6 +3,22 @@
 -- flush privileges ;
 
 -- MySQL 8
+-- mall_shop user create
 create user mmon identified WITH mysql_native_password by 'mmon@@1234' ;
-grant all privileges on *.* to mmon@'%';
+grant all privileges on mall_shop.* to mmon@'%';
+
+-- diquest user create
+create user diquest identified WITH mysql_native_password by 'diquest@@1234' ;
+grant all privileges on diquest.* to diquest@'%';
+grant SELECT on mall_shop.* to diquest@'%';
+grant INSERT, UPDATE, DELETE on mall_shop.mariner_index_log to diquest@'%';
+
+-- mts user create
+create user mts identified WITH mysql_native_password by 'mts@@1234' ;
+grant all privileges on mts.* to mts@'%';
+
+-- coocon user create
+create user coocon identified WITH mysql_native_password by 'coocon@@1234' ;
+grant all privileges on coocon.* to coocon@'%';
+
 flush privileges;
